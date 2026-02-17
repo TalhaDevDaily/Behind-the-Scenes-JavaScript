a6d054ea4161ebc615d3f34e52c5db083029a5e0
TalhaDevDaily <talha@boishakhikunja.onmicrosoft.com>
2026-02-17 14:05:31 +0600

    Hoisting and TDZ

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+/*
+function calcAge(birthYear) {
+  const age = 2025 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1997 && birthYear <= 2012) {
+      var genZ = true;
+      //   Creating NEW variable with same name as outer scopes variable
+      const firstName = 'Jonas';
+      //   Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+
+      const str = `Oh, and you're a generation - z, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(genZ);
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Talha';
+calcAge(1998);
+*/
