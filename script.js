a6193fa252783e8411f7384e1990ada9b0b955d1
TalhaDevDaily <talha@boishakhikunja.onmicrosoft.com>
2026-02-20 13:38:28 +0600

    Done

--- script.js
+++ script.js
@@ -37,8 +37,8 @@ const firstName = 'Talha';
 calcAge(1998);
 */
 
+/*
 // Hoisting and TDZ in Practice
-
 // Variables
 console.log(me);
 // console.log(job);
@@ -76,3 +76,92 @@ function deleteShoppingCart() {
 var x = 1;
 let y = 2;
 const z = 3;
+*/
+
+// The this keyword in Practice
+/*
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1998);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1998);
+*/
+
+/*
+const talha = {
+  year: 1998,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+talha.calcAge();
+
+const jonas = {
+  year: 1991,
+};
+
+jonas.calcAge = talha.calcAge;
+jonas.calcAge();
+
+const f = talha.calcAge;
+// f();
+*/
+
+// Regular Functions vs. Arrow Functions
+// var firstName = 'Jonas';
+
+const talha = {
+  firstName: 'Talha',
+  year: 1998,
+  calcAge: function () {
+    // console.log(this);
+    // console.log(2037 - this.year);
+
+    /*
+    // Solution 1
+    const self = this; // self or that
+    const isGenZ = function () {
+      console.log(self);
+      console.log(self.year >= 1997 && self.year <= 2012);
+      console.log(this.year >= 1997 && this.year <= 2012);
+    };
+    */
+
+    // Solution 2
+    const isGenZ = () => {
+      // console.log(this);
+      // console.log(this.year >= 1997 && this.year <= 2012);
+    };
+    isGenZ();
+  },
+  greet: function () {
+    // console.log(this);
+    // console.log(`Hey my name is ${this.firstName}`);
+  },
+};
+// talha.greet();
+talha.calcAge();
+
+// Arguements keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+
+addArrow();
